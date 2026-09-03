65f29d64aa063dad4c34eb1639bac38d431e0365
Faheem <faheemakram2004@gmail.com>
2026-09-03 17:15:35 +0100

    Added Window Master task

---
+++ WindowMaster/src/WindowMaster.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class WindowMaster {
+    public static void main(String[] args) {
+
+        // Declare strings to be parsed
+        String stringHeight = "";
+        String stringWidth = "";
+
+        // Declare input variables
+        float height = 0;
+        float width = 0;
+
+
+        // Declare output variables
+        float area;
+        float perimeter;
+        float cost;
+
+        // Create new scanner instance
+        Scanner myScanner = new Scanner(System.in);
+
+        // Get valid input from user
+        boolean valid = false;
+        do {
+            try {
+                System.out.println("Enter the Height:");
+                stringHeight = myScanner.nextLine();
+                System.out.println("Enter the Width");
+                stringWidth = myScanner.nextLine();
+                height = Float.parseFloat(stringHeight);
+                width = Float.parseFloat(stringWidth);
+                valid = true;
+            } catch (NumberFormatException ex) {
+                System.out.println("Please enter valid numbers!");
+            }
+        } while (!valid);
+
+        // Calculate final output variables
+        area = height * width;
+        perimeter = 2*(height + width);
+        cost = (3.5f * area) + (2.25f * perimeter);
+
+        System.out.println("Window height = " + stringHeight);
+        System.out.println("Window width = " + stringWidth);
+        System.out.println("Window area = " + area);
+        System.out.println("Window perimeter = " + perimeter);
+        System.out.println("Total Cost =  " + cost);
+
+    }
+}
