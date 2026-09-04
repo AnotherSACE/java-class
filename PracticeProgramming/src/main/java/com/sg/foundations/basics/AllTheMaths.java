b7234843c9217e726c567df7742702e67d980843
Faheem <faheemakram2004@gmail.com>
2026-09-04 15:15:01 +0100

    Added some exercises including Rock Paper Scissors

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/AllTheMaths.java
@@ -0,0 +1,23 @@
+package com.sg.foundations.basics;
+
+public class AllTheMaths {
+    public static void main(String[] args) {
+        System.out.print("1 + 2 is: ");
+        System.out.println(1 +  2);
+
+        System.out.print("5565.0 divided by 22.9 is : ");
+        System.out.println("5565.0 / 22.0");
+
+        System.out.print("223 times 31 - 42: ");
+        System.out.println(223 * 31 - 42);
+
+        System.out.print("Is 4 greater than -1? ");
+        System.out.println(4 > -1);
+
+        System.out.println("\n****** Now make the computer do some harder math!");
+
+        System.out.print("8043.52 minus 4.2 plus 23.0 divided by 56.0 times -76.13 is: ");
+        System.out.println(8043.32 - 4.2 + 23.0 / 56.0 * -76.13);
+
+    }
+}
---
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/RockPaperScissors.java
@@ -0,0 +1,105 @@
+package com.sg.foundations.basics;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+
+    public static void main(String[] args) {
+
+        String userInput = "";
+        String computer = "";
+        boolean isValid = false;
+        boolean tryAgain = true;
+        int choice = 0;
+        int wins = 0, losses = 0, ties = 0;
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        // Loop through the puzzle
+        while (tryAgain){
+
+            // Ask user for their choice
+            isValid = false; // Reset in case the user enters something wrong on the second loop
+            do {
+                try {
+                    System.out.println("Rock (1), Paper (2) or Scissors (3?");
+                    userInput = sc.nextLine();
+                    if (!(userInput.equals("rock") ||
+                            userInput.equals("paper") ||
+                            userInput.equals("scissors")))
+                        throw new IllegalArgumentException("Invalid input");
+                    isValid = true;
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Please enter either 'rock' 'paper' or 'scissors'");
+                }
+                } while (!isValid);
+
+            // Generate a random choice between rock, paper and scissors
+            choice = rand.nextInt(3);
+            switch (choice) {
+                case 0:
+                    computer = "rock";
+                    System.out.println("Computer throws Rock");
+                    break;
+                case 1:
+                    computer = "paper";
+                    System.out.println("Computer throws Paper");
+                    break;
+                case 2:
+                    computer = "scissors";
+                    System.out.println("Computer throws Scissors");
+                    break;
+            }
+
+
+            // Check for a tie
+            if (computer.equals(userInput)) {
+                ties++;
+                System.out.println("Tie, try again? (Y/N)");
+            }
+            // Check if the user wins
+            else if (userInput.equals("rock") && computer.equals("scissors") ||
+                        userInput.equals("paper") && computer.equals("rock") ||
+                        userInput.equals("scissors") && computer.equals("paper")) {
+                wins++;
+                System.out.println("You Win! Play again? (Y/N)");
+            }
+
+            // Otherwise, the user lost
+            else {
+                losses++;
+                System.out.println("You Lose :( Play again? (Y/N)");
+            }
+
+            // Now check if the user wants to play again
+            isValid = false;
+            while (!isValid){
+                try {
+                    userInput = sc.nextLine();
+                    switch (userInput) {
+                        case "y":
+                            isValid = true;
+                            break;
+                        case "n":
+                            isValid = true;
+                            tryAgain = false;
+                            break;
+                        default:
+                            throw new IllegalArgumentException("Invalid input");
+                    }
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Please enter either 'y' or 'n'");
+                }
+            }
+        }
+
+        // Prints the wins at the end of the game
+        System.out.println("Wins: " + wins + " \nLosses: " + losses + "\nTies: " + ties);
+        if (wins > losses) System.out.println("Congrats, you're the overall winner!");
+        else if (wins < losses) System.out.println("Aww, better luck next time!");
+        else System.out.println("Looks like we tied...");
+        System.out.println("Thanks for playing!");
+        }
+    }
+}
