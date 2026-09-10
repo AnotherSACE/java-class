d53cb95f9786df565e42eacefd84a5e1e172dfc6
Faheem <faheemakram2004@gmail.com>
2026-09-10 01:48:07 +0100

    Added options for how interest is given out

--- Week 1/PracticeExercise/untitled/src/main/java/InterestCalculator.java
+++ Week 1/PracticeExercise/untitled/src/main/java/InterestCalculator.java
@@ -5,10 +5,11 @@ public class InterestCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float currentBalance = 0;
-        float quarterlyInterestRate = 0;
+        float interestRate = 0;
         int years = 0;
         boolean valid = false;
         float earned = 0;
+        int type = 0;
 
         do {
             try {
@@ -17,7 +18,32 @@ public class InterestCalculator {
                 System.out.print("How many years are investing? ");
                 years =Integer.parseInt(sc.nextLine());
                 System.out.print("What is the annual interest rate % growth? ");
-                quarterlyInterestRate = Float.parseFloat(sc.nextLine()) / 4;
+                interestRate = Float.parseFloat(sc.nextLine());
+                System.out.println("Is interest added: daily (1), weekly (2), monthly (3), quarterly (4), or yearly (5)");
+                type = Integer.parseInt(sc.nextLine());
+                switch (type) {
+                    case (1):
+                        System.out.println("You have selected daily compound interest");
+                        type = 365; // Could be 365.25, but I decided to make things simpler
+                        break;
+                    case (2):
+                        System.out.println("You have selected weekly compound interest");
+                        type = 51;
+                        break;
+                    case (3):
+                        System.out.println("You have selected monthly compound interest");
+                        type = 12;
+                        break;
+                    case (4):
+                        System.out.println("You have selected quarterly compound interest");
+                        break;
+                    case (5):
+                        System.out.println("You have selected annual compound interest");
+                        type = 1;
+                        break;
+                    default:
+                        throw new NumberFormatException();
+                }
                 valid = true;
             } catch (NumberFormatException e){
                 System.out.println("Please enter valid numbers!");
@@ -30,8 +56,8 @@ public class InterestCalculator {
             System.out.println("Year " + i + ":");
             System.out.println("Began with $" + currentBalance);
             earned = currentBalance;
-            for (int j = 0; j < 4; j++)
-                earned *= (1 + (quarterlyInterestRate / 100));
+            for (int j = 0; j < type; j++)
+                earned *= (1 + ((interestRate/type) / 100));
             earned -= currentBalance;
             System.out.println("Earned: $" + earned);
             currentBalance += earned;
